d114062fc05bd94197e81cfefff0d5c194353b1d
mpnsk <manuel.paunoski@stud.fh-rosenheim.de>
2015-12-02 23:42:28 +0100

    init

---
+++ src/main/java/com/mycompany/javadb/DerbyTest.java
@@ -0,0 +1,60 @@
+package com.mycompany.javadb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class DerbyTest {
+  private Connection connect = null;
+  private Statement statement = null;
+  private ResultSet resultSet = null;
+
+  
+  public DerbyTest() throws Exception {
+    try {
+
+      Class.forName("org.apache.derby.jdbc.ClientDriver").newInstance();
+      connect = DriverManager
+          .getConnection("jdbc:derby://localhost:1527/sample;user=app;password=app");
+      PreparedStatement statement = connect
+          .prepareStatement("SELECT * from APP.CUSTOMER");
+
+      resultSet = statement.executeQuery();
+      while (resultSet.next()) {
+        String user = resultSet.getString("NAME");
+        String number = resultSet.getString("ZIP");
+        System.out.println("User: " + user);
+        System.out.println("ID: " + number);
+      }
+    } catch (Exception e) {
+      throw e;
+    } finally {
+      close();
+    }
+
+  }
+
+  private void close() {
+    try {
+      if (resultSet != null) {
+        resultSet.close();
+      }
+      if (statement != null) {
+        statement.close();
+      }
+      if (connect != null) {
+        connect.close();
+      }
+    } catch (Exception e) {
+
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    DerbyTest dao = new DerbyTest();
+  }
+
+} 
+
